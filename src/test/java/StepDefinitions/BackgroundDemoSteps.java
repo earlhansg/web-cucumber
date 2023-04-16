e1ca0da537cbbb8335b512254f69ca26e1eb3247
Earl Hans Genoso <earlhansg@gmail.com>
2023-04-17 01:50:39 +0800

    T6-added BackgroundDemo.feature and BackgroundDemoSteps

---
+++ src/test/java/StepDefinitions/BackgroundDemoSteps.java
@@ -0,0 +1,54 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class BackgroundDemoSteps {
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user enters username and password")
+	public void user_enters_username_and_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("clicks on login button")
+	public void clicks_on_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user is navigated to the homepage")
+	public void user_is_navigated_to_the_homepage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user clicks on welcome link")
+	public void user_clicks_on_welcome_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("logout link is displayed")
+	public void logout_link_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user clicks on dashboard link")
+	public void user_clicks_on_dashboard_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("quick launch toolbar is displayed")
+	public void quick_launch_toolbar_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
